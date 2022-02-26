0b0aafcca902ef1ec5de5f2f868a6ff0662749a6
anelinikolozi <71875463+anelinikolozi@users.noreply.github.com>
2022-02-26 18:02:41 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import 'bootstrap'
+import StudentTable from "./component/StudentTable";
+import TeacherTable from "./component/TeacherTable";
+import NameForm from "./component/NameForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/student">student</Link>
+              </li>
+              <li>
+                <Link to="/teacher">teacher</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Routes>
+            <Route path="/student" element={<StudentTable></StudentTable>}>
+
+            </Route>
+            <Route path="/teacher" element={<TeacherTable></TeacherTable>}>
+
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+      <NameForm>
+
+      </NameForm>
     </div>
   );
 }
